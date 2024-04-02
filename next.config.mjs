dc2cb4112402f5fd18fab4b39c5b18c50bc931be
m-kyu <yicha7@gmail.com>
2024-04-02 14:52:36 +0900

    sdfsdf

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 const nextConfig = {
-    source: '/assetlinks.json',
-    headers: [
-      {
-        key: 'Content-Type',
-        value: 'application/json',
-      },
-    ],
+    // source: '/assetlinks.json',
+    // headers: [
+    //   {
+    //     key: 'Content-Type',
+    //     value: 'application/json',
+    //   },
+    // ],
     reactStrictMode: false,
   };
  
